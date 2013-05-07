a563e179e5bdb6bcf90bdc597f1b225f6e7f9456
Boy Hagemann <boy@swis.nl>
2013-05-08 00:06:04 +0200

    updated the tree view

--- src/views/navigation/index.blade.php
+++ src/views/navigation/index.blade.php
@@ -1,7 +1,11 @@
 <h2>Navigation</h2>
 
-@foreach($tree as $root)
+<ul class="unstyled row-fluid">
+    @foreach($tree as $root)
     @foreach($root->getDescendantsAndSelf() as $node)
-    {{ str_repeat('-- ', $node->depth) }} {{ $node->page->title }}: {{ $node->depth }}<br>
+    <li class="offset{{ $node->depth }}">            
+        <i class="icon-file muted"></i> <a href="{{ URL::route('cms.pages.content', $node->page->id) }}">{{ $node->page->title }}</a>
+    </li>
     @endforeach
-@endforeach
+    @endforeach
+</ul>
